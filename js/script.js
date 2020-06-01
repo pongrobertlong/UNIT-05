3406b35a37251f4add785da0e68092d2e21890ac
Robert Long <pongrobertlong@gmail.com>
2020-05-31 20:26:34 -0400

    JQuery over Vanilla JS
    - Used JQuery over Vanilla JS to create search function

--- js/script.js
+++ js/script.js
@@ -7,27 +7,11 @@ lightbox.option({
 
 // This code below returns the user search value
 
-const searchBar = document.getElementById('searchBar');
-const photoGallery = document.getElementById('photoGallery');
-const photoImg = document.querySelectorAll('a');
-const thumb = document.querySelectorAll('img');
-
-
-
-searchBar.addEventListener('keyup', (event)=> {
-    const searchValue = (event.target.value).toLowerCase();
-
-    
-    for (let i = 0; i < photoImg.length; i++ ) {
-        const photoCap = [photoImg[i].getAttribute('data-title').toLowerCase()];
-        
-        if (photoCap === searchValue) {
-            thumb[i].style.display = "";
-        } else {
-            thumb[i].style.display = "none";
-        }
-    }
-    
-});
-    
-
+$(document).ready(function(){
+    $("#searchBar").on("keyup", function() {
+      let value = $(this).val().toLowerCase();
+      $("#photoGallery a").filter(function() {
+        $(this).toggle($(this).attr('data-title').toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
